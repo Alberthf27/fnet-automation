f7cac53d59949ead336a879b4a3bc2a1039fdf9f
ALBERTH FLORES <alberthflores151@gmail.com>
2026-01-02 16:44:43 -0500

    Update SuscripcionDAO.java

--- src/main/java/DAO/SuscripcionDAO.java
+++ src/main/java/DAO/SuscripcionDAO.java
@@ -479,7 +479,7 @@ public class SuscripcionDAO {
         String sql = "SELECT c.dni FROM cliente c " +
                 "INNER JOIN suscripcion s ON s.id_cliente = c.id_cliente " +
                 "WHERE s.id_suscripcion = ?";
-        try (Connection con = Conexion.getConnection();
+try (Connection con = Conexion.getConexion();
                 PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, idSuscripcion);
             ResultSet rs = ps.executeQuery();
